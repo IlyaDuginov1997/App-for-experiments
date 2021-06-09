63fc72ed03212ea54906b80ce8920c695bd47b30
IlyaDuginov1997 <ilyaduginov1997@gmail.com>
2021-06-10 00:27:27 +0300

    LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,38 @@ function App() {
         setValue(value + 1)
     }
 
+    function setToLocalStorage() {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+
+    function getToLocalStorage() {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            const newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+
+    function clearLocalStorage() {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeLocalStorage = () => {
+        localStorage.removeItem('counterValue + 1')
+    }
+
     return (
         <div className='main'>
             <h3>
                 {value}
             </h3>
-            <button onClick={onClickButtonInc}>
-                inc
-            </button>
+            <button onClick={onClickButtonInc}> inc</button>
+            <button onClick={setToLocalStorage}> setToLocalStorage</button>
+            <button onClick={getToLocalStorage}> getToLocalStorage</button>
+            <button onClick={clearLocalStorage}> clearLocalStorage</button>
+            <button onClick={removeLocalStorage}> removeLocalStorage</button>
         </div>
     )
 }
